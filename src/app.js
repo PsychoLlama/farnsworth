59d8e24115650d3dbef1ce8c0e8ef5af3e6e6952
Jesse Gibson <JesseTheGibson@gmail.com>
2021-02-17 21:26:30 -0800

    Add React

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('Hello, world');
+import DOM from 'react-dom';
+import React from 'react';
+
+DOM.render(
+  <React.StrictMode>
+    <h1>Hello, world</h1>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
