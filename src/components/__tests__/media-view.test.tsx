ef873a353fae5b988820a485336526766f11371f
Jesse Gibson <JesseTheGibson@gmail.com>
2021-02-25 18:37:00 -0800

    Render media streams as they become available
    This binds the `MediaView` component to a set of tracks and a media
    stream. As you update the audio/video tracks, the media stream updates
    in turn.
    
    This could come back to bite me if I re-arrange the video layout. It's
    possible that the media stream and video may need to persist on the page
    to avoid audio gaps. That requires more manual testing.

--- src/components/__tests__/media-view.test.tsx
+++ src/components/__tests__/media-view.test.tsx
@@ -1,5 +1,6 @@
 import renderer from '../../testing/renderer';
 import MediaView from '../media-view';
+import context from '../../conferencing/global-context';
 
 describe('MediaView', () => {
   const setup = renderer(MediaView, {
@@ -9,7 +10,61 @@ describe('MediaView', () => {
     }),
   });
 
-  it('renders', () => {
-    expect(setup).not.toThrow();
+  function setVideoRef(
+    output: ReturnType<typeof setup>['output'],
+    ref = { srcObject: null, play: jest.fn() },
+  ) {
+    const video = output.find({ 'data-test-id': 'video-stream' }).getElement();
+    (video as any).ref(ref);
+
+    return ref;
+  }
+
+  it('shows the current video stream', () => {
+    const { output } = setup();
+    const ref = setVideoRef(output);
+
+    expect(ref.srcObject).toBeInstanceOf(MediaStream);
+  });
+
+  it('survives while unmounting', () => {
+    const { output } = setup();
+    const pass = () => setVideoRef(output, null);
+
+    expect(pass).not.toThrow();
+  });
+
+  it('binds tracks to the media stream', () => {
+    const audioTrack = new MediaStreamTrack();
+    const videoTrack = new MediaStreamTrack();
+    context.tracks.set('a-id', audioTrack);
+    context.tracks.set('v-id', videoTrack);
+
+    const { output } = setup({
+      audioTrackId: 'a-id',
+      videoTrackId: 'v-id',
+    });
+
+    const ref = setVideoRef(output);
+
+    expect(ref.srcObject.getTracks()).toContain(audioTrack);
+    expect(ref.srcObject.getTracks()).toContain(videoTrack);
+  });
+
+  it('removes tracks that are no longer needed', () => {
+    const audioTrack = new MediaStreamTrack();
+    context.tracks.set('a-id', audioTrack);
+    context.tracks.set('v-id', new MediaStreamTrack());
+
+    const { output } = setup({
+      audioTrackId: 'a-id',
+      videoTrackId: 'v-id',
+    });
+
+    const ref = setVideoRef(output);
+    output.setProps({ videoTrackId: null });
+
+    expect(ref.srcObject.getTracks()).toHaveLength(1);
+    expect(ref.play).toHaveBeenCalled();
   });
 });
--- src/components/media-view.tsx
+++ src/components/media-view.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+import context from '../conferencing/global-context';
 
 export default class MediaView extends React.Component<Props> {
-  render() {
-    return null;
+  stream = new MediaStream();
+  videoRef: null | HTMLVideoElement = null;
+
+  componentDidMount() {
+    this.syncTracksToMediaStream();
   }
+
+  componentDidUpdate() {
+    this.syncTracksToMediaStream();
+  }
+
+  render() {
+    return (
+      <VideoStream ref={this.attachMediaStream} data-test-id="video-stream" />
+    );
+  }
+
+  attachMediaStream = (video: null | HTMLVideoElement) => {
+    if (video) video.srcObject = this.stream;
+    this.videoRef = video;
+  };
+
+  // Ensures the media stream has the tracks described by props, and only
+  // those tracks. Any extras will be removed.
+  syncTracksToMediaStream = () => {
+    const { audioTrackId, videoTrackId } = this.props;
+    const trackIds = [audioTrackId, videoTrackId].filter(Boolean);
+    const tracks = trackIds.map((trackId) => context.tracks.get(trackId));
+    tracks.forEach((track) => this.stream.addTrack(track));
+
+    this.stream.getTracks().forEach((track) => {
+      if (!tracks.includes(track)) this.stream.removeTrack(track);
+    });
+
+    if (this.videoRef) this.videoRef.play();
+  };
 }
+
+const VideoStream = styled.video`
+  // TODO
+`;
 
 interface Props {
   audioTrackId: null | string;
