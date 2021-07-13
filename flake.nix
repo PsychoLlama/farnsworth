5909173518e18d219cc1d174d069b053bebc2abb
Jesse Gibson <JesseTheGibson@gmail.com>
2021-07-12 18:25:34 -0700

    Add nix environment to support install deps
    `node-gyp` doesn't work without python installed. I don't have it
    installed by default.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in {
+      # Use `nix develop` to enter the dev environment.
+      devShell.${system} = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.python3];
+      };
+    };
+}
