16d422ceae7d628f983205e38c1716e0a97ba494
Jesse Gibson <JesseTheGibson@gmail.com>
2021-09-03 21:24:48 -0700

    Fix test by adding missing parameter
    I don't run the tests in CI.

--- src/effects/__tests__/connections.test.tsx
+++ src/effects/__tests__/connections.test.tsx
@@ -5,8 +5,9 @@ jest.mock('libp2p');
 describe('Connection effects', () => {
   describe('listen', () => {
     it('returns the peer ID', async () => {
-      await expect(connections.listen()).resolves.toEqual({
+      await expect(connections.listen('/rfc1149')).resolves.toEqual({
         id: 'mock-peer-id',
+        relayAddr: '/rfc1149',
       });
     });
   });
