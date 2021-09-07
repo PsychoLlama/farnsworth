8138cda9f28177de1b7fc7bfd99fbcba32ba5588
Jesse Gibson <JesseTheGibson@gmail.com>
2021-09-07 14:23:59 -0700

    Add middleware to pull state from redux
    I want to avoid referencing the redux singleton in my action effects.
    Some effects are triggered outside component lifecycles (e.g. WebRTC
    events) and need variables from the store to operate. This provides
    a mechanism that isn't completely untestable, isn't coupled to a store
    singleton, and plays well with other code.

--- src/utils/create-store.tsx
+++ src/utils/create-store.tsx
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { middleware } from 'retreon';
+import { middleware as retreonMiddleware } from 'retreon';
+import { middleware as stateMiddleware } from './middleware/get-state';
 import * as reducers from '../reducers';
 import initialState, { State } from '../reducers/initial-state';
 
@@ -8,7 +9,9 @@ const DEVTOOLS_KEY = '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__';
 export default function initialize(state: State = initialState) {
   const reducer = combineReducers(reducers);
   const composeEnhancers = window[DEVTOOLS_KEY] || compose;
-  const enhancer = composeEnhancers(applyMiddleware(middleware));
+  const enhancer = composeEnhancers(
+    applyMiddleware(retreonMiddleware, stateMiddleware),
+  );
 
   return createStore(reducer, state, enhancer);
 }
