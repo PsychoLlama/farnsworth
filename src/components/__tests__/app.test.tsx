320d72509b2c2ceff29ee335a137a1b443dbd5f9
Jesse Gibson <JesseTheGibson@gmail.com>
2021-11-27 12:50:09 -0800

    Avoid initializing the app twice in development
    If the `<App>` component hot reloads and tries to re-initialize libp2p,
    it causes app errors and puts development in a bad state. I've just been
    reloading this entire time. It's annoying enough that I've finally fixed
    it.
    
    If the app is already initialized, just quit early.

--- src/components/__tests__/app.test.tsx
+++ src/components/__tests__/app.test.tsx
@@ -1,11 +1,14 @@
+import provide from 'immer';
 import renderer from '../../testing/renderer';
-import { App } from '../app';
+import { App, mapStateToProps } from '../app';
+import initialState, { State } from '../../reducers/initial-state';
 
 describe('App', () => {
   const setup = renderer(App, {
     getDefaultProps: () => ({
       requestMediaDevices: jest.fn(),
       connectToServer: jest.fn(),
+      appInitialized: false,
     }),
   });
 
@@ -19,5 +22,37 @@ describe('App', () => {
     const { props } = setup();
 
     expect(props.connectToServer).toHaveBeenCalled();
+  });
+
+  it('does not initialize the application if already set up', () => {
+    const { props } = setup({ appInitialized: true });
+
+    expect(props.requestMediaDevices).not.toHaveBeenCalled();
+    expect(props.connectToServer).not.toHaveBeenCalled();
+  });
+
+  describe('mapStateToProps', () => {
+    function setup(patch: (state: State) => unknown) {
+      const state = provide(initialState, patch);
+      const props = mapStateToProps(state);
+
+      return {
+        props,
+        state,
+      };
+    }
+
+    it('returns the right props', () => {
+      const { props } = setup((state) => {
+        state.relay = {
+          server: '/server/multiaddr',
+          localId: 'my-local-id',
+        };
+      });
+
+      expect(props).toMatchObject({
+        appInitialized: true,
+      });
+    });
   });
 });
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,9 +5,17 @@ import * as actions from '../actions';
 import VideoRoomLayout from './media/video-room-layout';
 import { SERVER_ADDRESS } from '../utils/constants';
 import RouteObserver from './route-observer';
+import { State } from '../reducers/initial-state';
 
 export class App extends React.Component<Props> {
   async componentDidMount() {
+    // The only time this happens is during development, when the `<App>`
+    // component is hot reloaded. Trying to initialize the app again messes
+    // everything up and can cause errors.
+    if (this.props.appInitialized) {
+      return;
+    }
+
     this.props.requestMediaDevices();
 
     // Opens a connection to the relay server.
@@ -33,6 +41,14 @@ const Container = styled.div`
 interface Props {
   requestMediaDevices: typeof actions.devices.requestMediaDevices;
   connectToServer: typeof actions.connections.listen;
+  appInitialized: boolean;
+}
+
+export function mapStateToProps(state: State) {
+  return {
+    // This is a good approximation.
+    appInitialized: state.relay !== null,
+  };
 }
 
 const mapDispatchToProps = {
@@ -40,4 +56,4 @@ const mapDispatchToProps = {
   connectToServer: actions.connections.listen,
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
