d20458cab7a6fbc8227d62d56d3d8bb0c7b28ac1
Jesse Gibson <JesseTheGibson@gmail.com>
2021-12-01 19:34:13 -0800

    Show list of available devices in settings panel
    You can see the full list of audio and video devices reported by your
    system. You can even choose them! It doesn't do anything yet, but hey.

--- src/components/settings/__tests__/settings-panel.test.tsx
+++ src/components/settings/__tests__/settings-panel.test.tsx
@@ -1,14 +1,58 @@
+import { DeviceInfo, DeviceKind } from 'media-devices';
 import renderer from '../../../testing/renderer';
-import SettingsPanel from '../settings-panel';
+import { SettingsPanel, mapStateToProps } from '../settings-panel';
+import initialState from '../../../reducers/initial-state';
 
 describe('SettingsPanel', () => {
+  function createSource(override: Partial<DeviceInfo>): DeviceInfo {
+    return {
+      kind: DeviceKind.AudioInput,
+      label: 'Device Label',
+      groupId: 'group-id',
+      deviceId: 'device-id',
+      ...override,
+    };
+  }
+
   const setup = renderer(SettingsPanel, {
     getDefaultProps: () => ({
-      // TODO
+      audioSources: [createSource({ kind: DeviceKind.AudioInput })],
+      videoSources: [
+        createSource({ kind: DeviceKind.VideoInput }),
+        createSource({ kind: DeviceKind.VideoInput }),
+      ],
     }),
   });
 
-  it('renders', () => {
-    expect(setup).not.toThrow();
+  it('renders an option for every device', () => {
+    const { findByTestId, props } = setup();
+
+    expect(findByTestId('audio-source-option').length).toBe(
+      props.audioSources.length,
+    );
+
+    expect(findByTestId('video-source-option').length).toBe(
+      props.videoSources.length,
+    );
+  });
+
+  it('disables the inputs if there are no options', () => {
+    const { findByTestId } = setup({ audioSources: [], videoSources: [] });
+
+    expect(findByTestId('choose-audio-source').prop('disabled')).toBe(true);
+    expect(findByTestId('choose-video-source').prop('disabled')).toBe(true);
+  });
+
+  describe('mapStateToProps', () => {
+    it('returns the expected props', () => {
+      const props = mapStateToProps(initialState);
+
+      expect(props).toMatchInlineSnapshot(`
+        Object {
+          "audioSources": Array [],
+          "videoSources": Array [],
+        }
+      `);
+    });
   });
 });
--- src/components/settings/settings-panel.tsx
+++ src/components/settings/settings-panel.tsx
@@ -1,5 +1,118 @@
+import { DeviceInfo } from 'media-devices';
 import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { v4 as uuid } from 'uuid';
+import { State } from '../../reducers/initial-state';
+import * as css from '../../utils/css';
 
-export default function SettingsPanel() {
-  return <p>Future home of settings</p>;
+export class SettingsPanel extends React.Component<Props> {
+  audioInputId = uuid();
+  videoInputId = uuid();
+
+  render() {
+    const { audioSources, videoSources } = this.props;
+
+    return (
+      <Container>
+        <InputGroup>
+          <InputTitle htmlFor={this.audioInputId}>Audio devices</InputTitle>
+          <Dropdown
+            data-test="choose-audio-source"
+            id={this.audioInputId}
+            disabled={audioSources.length === 0}
+          >
+            {this.props.audioSources.map(this.renderAudioOption)}
+          </Dropdown>
+        </InputGroup>
+
+        <InputGroup>
+          <InputTitle htmlFor={this.videoInputId}>Video devices</InputTitle>
+          <Dropdown
+            data-test="choose-video-source"
+            id={this.videoInputId}
+            disabled={videoSources.length === 0}
+          >
+            {this.props.videoSources.map(this.renderVideoOption)}
+          </Dropdown>
+        </InputGroup>
+      </Container>
+    );
+  }
+
+  renderAudioOption = (device: DeviceInfo) => {
+    return (
+      <option key={this.getId(device)} data-test="audio-source-option">
+        {device.label}
+      </option>
+    );
+  };
+
+  renderVideoOption = (device: DeviceInfo) => {
+    return (
+      <option key={this.getId(device)} data-test="video-source-option">
+        {device.label}
+      </option>
+    );
+  };
+
+  getId = (device: DeviceInfo) => {
+    return `${device.groupId}:${device.deviceId}`;
+  };
 }
+
+interface Props {
+  audioSources: Array<DeviceInfo>;
+  videoSources: Array<DeviceInfo>;
+}
+
+const Container = styled.div`
+  display: grid;
+  grid-row-gap: 2rem;
+  padding: 1rem;
+  max-width: 315px;
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+const InputTitle = styled.label`
+  width: max-content;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+// Hold my beer.
+const Dropdown = styled.select`
+  appearance: none;
+  border-radius: ${css.radius};
+  border: 1px solid ${css.color('foreground')};
+  background-color: ${css.color('background')};
+  box-sizing: border-box;
+  padding: 0.5rem 0.25rem;
+  color: ${css.color('text')};
+  font-size: 85%;
+
+  :hover:not(:disabled),
+  :focus:not(:disabled) {
+    border-color: ${css.color('primary')};
+    outline: none;
+  }
+
+  :disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+export function mapStateToProps(state: State) {
+  return {
+    audioSources: state.sources.available.audio,
+    videoSources: state.sources.available.video,
+  };
+}
+
+export default connect(mapStateToProps)(SettingsPanel);
