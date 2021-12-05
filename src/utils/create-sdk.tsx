0ebd404a56ae77bbab548425aef9a833662bea79
Jesse Gibson <JesseTheGibson@gmail.com>
2021-12-05 15:50:43 -0800

    Correct SDK type signature
    The SDK wasn't correctly inferring dispatch return types. It was causing
    false warnings in my tests.

--- src/utils/create-sdk.tsx
+++ src/utils/create-sdk.tsx
@@ -10,14 +10,29 @@ import { State } from '../reducers/initial-state';
  * This enables conferencing logic to emit events without coupling to Redux.
  */
 export default function createSdk(reduxStore: Store<State>): SDK {
-  return Object.entries(actions).reduce(
-    (sdk, [name, creators]) => ({
+  return Object.entries(actions).reduce((sdk, [name, creators]) => {
+    return {
       ...sdk,
       [name]: bindActionCreators(creators, reduxStore.dispatch),
-    }),
-    {} as SDK,
-  );
+    };
+  }, {} as SDK);
 }
 
-// Same signature, just wrapped with `dispatch()`.
-type SDK = typeof actions;
+type Actions = typeof actions;
+
+// Retreon uses generator functions for all action creators. The SDK
+// transforms their type signatures into (args) => Dispatch<T>, which is the
+// equivalent of `bindActionCreators(...)`.
+type SDK = {
+  [ToolkitName in keyof Actions]: {
+    [CreatorName in keyof Actions[ToolkitName]]: Actions[ToolkitName][CreatorName] extends (
+      ...args: infer Args
+    ) => AsyncGenerator<unknown, infer R>
+      ? (...args: Args) => Promise<R>
+      : Actions[ToolkitName][CreatorName] extends (
+          ...args: infer Args
+        ) => Generator<unknown, infer R>
+      ? (...args: Args) => R
+      : never;
+  };
+};
